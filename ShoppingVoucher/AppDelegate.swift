a7845f921be9a46a2ea9f12c3985aedabdb8b57b
lumangmang <774343332@qq.com>
2019-11-13 13:23:32 +0800

    init

---
+++ ShoppingVoucher/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  ShoppingVoucher
+//
+//  Created by Mac on 2019/11/13.
+//  Copyright © 2019 Mac. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ ShoppingVoucher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShoppingVoucher
+//
+//  Created by Mac on 2019/11/13.
+//  Copyright © 2019 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
